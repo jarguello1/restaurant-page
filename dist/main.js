55b6b4ade8ba2b83d8b8c66ee38eb8fbb7c43c00
jarguello1 <jcarguello11@live.com>
2022-09-01 14:46:10 -0400

    add navigation buttons

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,32 @@
-(()=>{const e=document.querySelector("#content"),t=document.createElement("header"),n=document.createElement("div"),d=document.createElement("footer");t.classList.add("header"),t.textContent="Fritanga",e.appendChild(t),n.classList.add("main"),n.textContent="Middle Bit",e.appendChild(n),d.classList.add("footer"),d.textContent=`Copyright © ${(new Date).getFullYear()} josearguello`,e.appendChild(d)})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("const content = document.querySelector('#content');\nconst header = document.createElement('header');\nconst main = document.createElement('div');\nconst footer = document.createElement('footer');\n\n//create header\nfunction createHeader() {\n\n    header.classList.add('header');\n    header.textContent = \"Fritanga\";\n    \n    const navBar = document.createElement('div');\n    navBar.classList.add('navBar');\n    header.appendChild(navBar);\n\n    //create Nav Buttons\n    navList = ['home', 'menu', 'contact'];\n    navList.forEach(function(e) {\n        const navBtn = document.createElement('button');\n        navBtn.classList.add('navBtn');\n        navBtn.setAttribute('id', e);\n        navBtn.textContent = e;\n        header.appendChild(navBtn);\n    })\n\n    content.appendChild(header);\n}\n\n//create main body\nfunction createMain() {\n    main.classList.add('main');\n    main.textContent = \"Middle Bit\";\n    content.appendChild(main);\n}\n\n//create footer\nfunction createFooter() {\n    footer.classList.add('footer');\n    footer.textContent = `Copyright © ${new Date().getFullYear()} josearguello`;\n    content.appendChild(footer);\n}\n\n(function () {\n    createHeader();\n    createMain();\n    createFooter();\n})();\n\n\nconst homeBtn = document.getElementById('home');\nhomeBtn.textContent = \"Home\"\n//homeBtn.addEventListener('click', homePage);\n\nconst menuBtn = document.getElementById('menu');\nmenuBtn.textContent = 'Menu';\n//menuBtn.addEventListener('click', menuPage);\n\nconst contactBtn = document.getElementById('contact');\ncontactBtn.textContent = \"Contact\";\n//contactBtn.addEventListener('click', contactPage);\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,57 @@ const main = document.createElement('div');
 const footer = document.createElement('footer');
 
 //create header
-header.classList.add('header');
-header.textContent = "Fritanga";
-content.appendChild(header);
+function createHeader() {
 
-main.classList.add('main');
-main.textContent = "Middle Bit";
-content.appendChild(main);
+    header.classList.add('header');
+    header.textContent = "Fritanga";
+    
+    const navBar = document.createElement('div');
+    navBar.classList.add('navBar');
+    header.appendChild(navBar);
 
-footer.classList.add('footer');
-footer.textContent = `Copyright © ${new Date().getFullYear()} josearguello`;
-content.appendChild(footer);
+    //create Nav Buttons
+    navList = ['home', 'menu', 'contact'];
+    navList.forEach(function(e) {
+        const navBtn = document.createElement('button');
+        navBtn.classList.add('navBtn');
+        navBtn.setAttribute('id', e);
+        navBtn.textContent = e;
+        header.appendChild(navBtn);
+    })
+
+    content.appendChild(header);
+}
+
+//create main body
+function createMain() {
+    main.classList.add('main');
+    main.textContent = "Middle Bit";
+    content.appendChild(main);
+}
+
+//create footer
+function createFooter() {
+    footer.classList.add('footer');
+    footer.textContent = `Copyright © ${new Date().getFullYear()} josearguello`;
+    content.appendChild(footer);
+}
+
+(function () {
+    createHeader();
+    createMain();
+    createFooter();
+})();
+
+
+const homeBtn = document.getElementById('home');
+homeBtn.textContent = "Home"
+//homeBtn.addEventListener('click', homePage);
+
+const menuBtn = document.getElementById('menu');
+menuBtn.textContent = 'Menu';
+//menuBtn.addEventListener('click', menuPage);
+
+const contactBtn = document.getElementById('contact');
+contactBtn.textContent = "Contact";
+//contactBtn.addEventListener('click', contactPage);
